26924aa0d2093aa1bd9492b12e20d473b6370682
Mindi <Mindii@users.noreply.github.com>
2018-10-24 21:27:50 +0300

    Add files via upload

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,5 +1,5 @@
 -------------------------------------------------------------------------------
--- Whirange	AwesomeWM -	Not	so Awesome,	Awesome	Dot
+-- Whirange	AwesomeWM - Not so Awesome, Awesome Dots
 -- Mindi @ Mindinet.org
 -- GNU General Public License v3.0
 -------------------------------------------------------------------------------
@@ -40,13 +40,13 @@ end
 -- Variables
 -------------------------------------------------------------------------------
 beautiful.init(awful.util.getdir("config") .. "/themes/whirange/theme.lua")
-Mindi =	{}
+Mindi = {}
 Mindi.Prog = {}
-Mindi.Command =	{}
+Mindi.Command = {}
 Mindi.Path = {}
-Mindi.Bar =	{}
+Mindi.Bar = {}
 -- Main	Vars
-Mindi.Mod =					'Mod4' -- Mod key
+Mindi.Mod = 				'Mod4' -- Mod key
 Mindi.Tags1 = 				{"一", "二", "三", "四", "五", "六", "七", "八", "九"}
 Mindi.Tags2 = 				{"一", "二", "三", "四"}
 -- Programs
@@ -54,21 +54,21 @@ Mindi.Prog.Terminal = 		'urxvt'	-- Terminal command (Mod + Return)
 Mindi.Prog.FileManager = 	'thunar' -- Filemanager (Mod + E)
 Mindi.Prog.MusicPlayer = 	'mocp' -- Music	Player (Mod	+ M)
 -- Commands
-Mindi.Command.Screenshot =	awful.util.getdir("config") .. 'scripts/screenshot.sh' -- Screenshot Command (Print + Click on window)
-Mindi.Command.Autostart =	awful.util.getdir("config") .. 'scripts/autorun.sh' -- Autostart Script
-Mindi.Command.NPPaste =		awful.util.getdir("config") .. 'scripts/nowplay_paste'
-Mindi.Command.Buy =			awful.util.getdir("config") .. 'scripts/text_paste buy'
-Mindi.Command.Btw =			awful.util.getdir("config") .. 'scripts/text_paste btw'
+Mindi.Command.Screenshot = 	awful.util.getdir("config") .. 'scripts/screenshot.sh' -- Screenshot Command (Print + Click on window)
+Mindi.Command.Autostart = 	awful.util.getdir("config") .. 'scripts/autorun.sh' -- Autostart Script
+Mindi.Command.NPPaste = 	awful.util.getdir("config") .. 'scripts/nowplay_paste'
+Mindi.Command.Buy = 		awful.util.getdir("config") .. 'scripts/text_paste buy'
+Mindi.Command.Btw = 		awful.util.getdir("config") .. 'scripts/text_paste btw'
 -- Paths
-Mindi.Path.Icon =		 	awful.util.getdir("config")	.. "/themes/whirange/icon/"
+Mindi.Path.Icon = 			awful.util.getdir("config")	.. "/themes/whirange/icon/"
 -- Bar
-Mindi.Bar.Height =  		'16' -- Taskbar Height
-Mindi.Bar.BorderWidth =		'2'
-Mindi.Bar.Clock =   		require("widgets.clock")	--	Taskbar	Clock
-Mindi.Bar.Cpu =				require("widgets.cpu")
-Mindi.Bar.Mem =				require("widgets.mem")
-Mindi.Bar.Hdd =				require("widgets.hdd")
-Mindi.Bar.Weather =			require("widgets.weather")
+Mindi.Bar.Height = 			'16' -- Taskbar Height
+Mindi.Bar.BorderWidth = 	'2'
+Mindi.Bar.Clock = 			require("widgets.clock")	--	Taskbar	Clock
+Mindi.Bar.Cpu = 			require("widgets.cpu")
+Mindi.Bar.Mem = 			require("widgets.mem")
+Mindi.Bar.Hdd = 			require("widgets.hdd")
+Mindi.Bar.Weather = 		require("widgets.weather")
 
 -------------------------------------------------------------------------------
 -- Layout's
@@ -248,33 +248,33 @@ root.buttons(gears.table.join(
 -- Key Bindings
 -------------------------------------------------------------------------------
 globalkeys = gears.table.join(
-	awful.key({						  }, "Print", 		function() os.execute(Mindi.Command.Screenshot) end),
-	awful.key({ Mindi.Mod,			  }, "r",			function() awful.screen.focused().mypromptbox:run() end),
-	awful.key({ Mindi.Mod,			  }, "Return", 		function() awful.spawn(Mindi.Prog.Terminal) end),
-	awful.key({ Mindi.Mod,			  }, "e",			function() awful.spawn(Mindi.Prog.FileManager) end),
-	awful.key({ Mindi.Mod,			  }, "d",			function() menubar.show()	end),
+	awful.key({ 					  }, "Print",   	function() os.execute(Mindi.Command.Screenshot) end),
+	awful.key({ Mindi.Mod,			  }, "r",       	function() awful.screen.focused().mypromptbox:run() end),
+	awful.key({ Mindi.Mod,			  }, "Return",  	function() awful.spawn(Mindi.Prog.Terminal) end),
+	awful.key({ Mindi.Mod,			  }, "e",       	function() awful.spawn(Mindi.Prog.FileManager) end),
+	awful.key({ Mindi.Mod,			  }, "d",       	function() menubar.show()	end),
 	--	Select with	arrows
-	awful.key({ Mindi.Mod,			  }, "Up", 			function() awful.client.focus.global_bydirection("up") end),
+	awful.key({ Mindi.Mod,			  }, "Up",   		function() awful.client.focus.global_bydirection("up") end),
 	awful.key({ Mindi.Mod,			  }, "Down", 		function() awful.client.focus.global_bydirection("down") end),
 	awful.key({ Mindi.Mod,			  }, "Left", 		function() awful.client.focus.global_bydirection("left") end),
-	awful.key({ Mindi.Mod,			  }, "Right", 		function() awful.client.focus.global_bydirection("right")	end),
+	awful.key({ Mindi.Mod,			  }, "Right",		function() awful.client.focus.global_bydirection("right")	end),
 	--	Move with arrows
-	awful.key({ Mindi.Mod,	"Shift"	  }, "Up", 			function() awful.client.swap.global_bydirection("up")	end),
+	awful.key({ Mindi.Mod,	"Shift"	  }, "Up",   		function() awful.client.swap.global_bydirection("up")	end),
 	awful.key({ Mindi.Mod,	"Shift"	  }, "Down", 		function() awful.client.swap.global_bydirection("down") end),
 	awful.key({ Mindi.Mod,	"Shift"	  }, "Left", 		function() awful.client.swap.global_bydirection("left") end),
-	awful.key({ Mindi.Mod,	"Shift"	  }, "Right", 		function() awful.client.swap.global_bydirection("right") end),
+	awful.key({ Mindi.Mod,	"Shift"	  }, "Right",		function() awful.client.swap.global_bydirection("right") end),
 	--	Restart	/ Quit
-	awful.key({ Mindi.Mod,	"Control" }, "r", 			awesome.restart),
-	awful.key({ Mindi.Mod,	"Shift"	  }, "e", 			awesome.quit),
+	awful.key({ Mindi.Mod,	"Control" }, "r",    		awesome.restart),
+	awful.key({ Mindi.Mod,	"Shift"	  }, "e",    		awesome.quit),
 	-- Media Keys
-	awful.key({ Mindi.Mod,}, "m",						function() awful.spawn(Mindi.Prog.Terminal .. " -e " .. Mindi.Prog.MusicPlayer) end),
-	awful.key({ Mindi.Mod,}, "z",						function() os.execute(Mindi.Command.NPPaste) end),
+	awful.key({ Mindi.Mod,}, "m",        				function() awful.spawn(Mindi.Prog.Terminal .. " -e " .. Mindi.Prog.MusicPlayer) end),
+	awful.key({ Mindi.Mod,}, "z",        				function() os.execute(Mindi.Command.NPPaste) end),
 	awful.key({}, "XF86AudioLowerVolume",				function() awful.spawn('amixer set Master 3%-') end),
 	awful.key({}, "XF86AudioRaiseVolume",				function() awful.spawn('amixer set Master 3%+') end),
-	awful.key({}, "XF86AudioPlay",						function() awful.spawn('mocp --play') end),
-	awful.key({}, "XF86AudioStop",						function() awful.spawn('mocp --stop') end),
-	awful.key({}, "XF86AudioNext",						function() awful.spawn('mocp --next') end),
-	awful.key({}, "XF86AudioPrev",						function() awful.spawn('mocp --previous') end),
+	awful.key({}, "XF86AudioPlay",       				function() awful.spawn('mocp --play') end),
+	awful.key({}, "XF86AudioStop",       				function() awful.spawn('mocp --stop') end),
+	awful.key({}, "XF86AudioNext",       				function() awful.spawn('mocp --next') end),
+	awful.key({}, "XF86AudioPrev",       				function() awful.spawn('mocp --previous') end),
 	-- Other
 	awful.key({ Mindi.Mod, "Control" }, "j",			function() os.execute(Mindi.Command.Buy) end),
 	awful.key({ Mindi.Mod, "Control" }, "b",			function() os.execute(Mindi.Command.Btw) end)
