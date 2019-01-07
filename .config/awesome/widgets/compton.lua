362aa06dfd54babdcb84a6fff93d11e882088a86
Mindi <Mindii@users.noreply.github.com>
2019-01-07 03:03:00 +0200

    Toggle Buttons + Compton Enabled

---
+++ .config/awesome/widgets/compton.lua
@@ -0,0 +1,35 @@
+-------------------------------------------------------------------------------
+-- Whirange	AwesomeWM Widget Compton toggle
+-- Mindi @ Mindinet.org
+-- GNU General Public License v3.0
+-------------------------------------------------------------------------------
+local gears	= require("gears")
+local awful   = require("awful")
+local wibox   = require("wibox")
+local beautiful	= require("beautiful")
+local toggle = 'sh /home/mindi/.config/awesome/scripts/toggle compton'
+local compton = {}
+
+local icon = '<span color="' .. beautiful.widget_icon_color .. '" font="fontawesome ' .. beautiful.widget_icon_size .. '"></span>'
+local icon_no = '<span color="' .. beautiful.widget_icon_off_color .. '" font="fontawesome ' .. beautiful.widget_icon_size .. '"></span>'
+
+local compton = awful.widget.watch('sh /home/mindi/.config/awesome/scripts/running compton', 10, function(widget, stdout) 
+	if stdout:match("yes") then
+		widget:set_markup(" " .. icon .. " ")
+	else
+		widget:set_markup(" " .. icon_no .. " ")
+	end
+end)
+
+compton:connect_signal("button::press", function(_,_,_,button)
+    if (button == 1)     then awful.spawn(toggle)
+    end
+end)
+
+return {
+	compton, 
+	bg = beautiful.taglist_bg_empty, 
+	fg = beautiful.taglist_fg_empty,
+	shape = gears.shape.partially_rounded_rect,
+	widget = wibox.container.background, 
+}
